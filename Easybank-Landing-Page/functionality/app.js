3b06f1a511ddfb828a145a450988f1b3c1f36202
John <jmpwonson@gmail.com>
2020-09-16 19:35:26 +1000

    Create app.js

---
+++ Easybank-Landing-Page/functionality/app.js
@@ -0,0 +1,15 @@
+const mobileMenuButton = document.getElementById("mobile-menu-button");
+const mobileMenu = document.getElementById("mobile-menu");
+const navItem = document.getElementById("nav-item");
+
+let mobileMenuVisible = false;
+
+mobileMenuButton.addEventListener("click", () => {
+    if (mobileMenuVisible) {
+        mobileMenu.style.display = "none";
+    } else {
+        mobileMenu.style.display = "flex";
+    }
+
+    mobileMenuVisible = !mobileMenuVisible;
+});
